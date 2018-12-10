de0d0e090296a41603dac91430034c11f0155ecd
markdan58 <mark.dan@strathmore.edu>
2018-12-10 22:20:16 +0300

    added url patterns

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,3 +3,13 @@ from . import views
 import datetime as dt
 from django.conf.urls.static import static
 from django.conf import settings
+
+urlpatterns=[
+    url(r'^$',views.displayphoto,name= 'displayphoto'), 
+    url(r'^details/(\d+)',views.details,name= 'details'), 
+    url(r'^search/',views.search_results,name= 'search_results'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
